b24f2f8f3c8ca8c5050d4e3c7263a8c153df8a01
Oleg Klimin <oleg.v.klimin@gmail.com>
2016-09-03 20:02:20 +0300

    add script for creating admin user

---
+++ addAdminUser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adminName=$1
+# You need to be SUDO using "sudo -i" not "sudo -s".
+
+groupadd $adminName
+useradd $adminName -U -m -G adm,cdrom,sudo,dip,plugdev,lxd,lpadmin,sambashare
+
+mkdir /home/$adminName/.ssh
+cp authorizedKeys.$adminName* /home/$adminName/.ssh/authorized_keys
+chown $adminName:$adminName /home/$adminName/.ssh/authorized_keys
+chmod 600 /home/$adminName/.ssh/authorized_keys
+
